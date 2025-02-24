412d67701555ffa293de0efc837a17d9419ce3c5
Anand <phadnisanandhyd@gmail.com>
2025-02-24 10:41:17 +0530

    commit

---
+++ desc2.js
@@ -0,0 +1,31 @@
+// const arr = [10,20,40,50,90];
+// arr.splice(2, 0 , 30, 35)
+// console.log(arr);
+
+
+// const arr = [10,20,40,50,90];
+// const newArr = arr.slice(2,3);
+// console.log(newArr);
+
+// const students = [
+//   { name: 'Quincy', grade: 96 },
+//   { name: 'Jason', grade: 84 },
+//   { name: 'Alexis', grade: 100 },
+//   { name: 'Sam', grade: 65 },
+//   { name: 'Katie', grade: 90 }
+// ];
+
+// const newEle = { name: 'Anand', grade: 80 };
+// console.log(students);
+// const secondArr = [...students , newEle ]
+// console.log(secondArr);
+
+// const arr = [10,20,40,50,90];
+// const newEle = 70;
+// const newArr = [...arr , newEle];
+// console.log(newArr);
+
+// const arr = [10,20,40,50,90];
+// arr.unshift(5);
+// arr.push(100);
+// console.log(arr);
